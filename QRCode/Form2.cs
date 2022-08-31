22eff4e750308a300b89a661e6f59eba0858f160
Haşim AKÇAY <77328571+hasimakcy@users.noreply.github.com>
2022-08-31 12:07:39 +0300

    Add files via upload

---
+++ QRCode/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode;
+using MessagingToolkit.QRCode.Codec;
+
+namespace QRCode
+{
+    public partial class Form2 : Form
+    {
+        QRCodeEncoder code = new QRCodeEncoder();
+        Image resim;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resim = code.Encode(textBox1.Text);
+            pictureBox1.Image = resim;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Form1 ekle = new Form1();
+            //ekle.Show();
+            //this.Hide();
+        }
+    }
+}
